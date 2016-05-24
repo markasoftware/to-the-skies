a4590d290e6f6b1045ddc8ffa8dc7f4c51843255
Mark Polyakov <spamhereplease@markasoftware.com>
2016-05-24 12:54:13 -0700

    random stuff from long ago

--- src/scripts/index-caller.js
+++
@@ -1,3 +0,0 @@
-//This file calls the functions set up by other files
-
-menuManager.registerEventListeners();
--- test/test.js
+++ test/test.js
@@ -3,6 +3,11 @@ var sinon = require('sinon');
 require('jsdom-global')();
 var fs = require('fs');
 var path = require('path');
+var supertest = require('supertest');
+var pg = require('pg');
+
+//setup database
+require('../setup/db.js').setCb(function(){
 
 describe('client side',function(){
 
@@ -11,7 +16,7 @@ var $ = function(){
 }
 
 function readScriptSync(fileName){
-    return fs.readFileSync(path.join(__dirname, '../src/scripts/', fileName), 'utf8');
+    return fs.readFileSync(path.join(__dirname, '../src/public/scripts/', fileName), 'utf8');
 }
 
 function clearBody(){
@@ -79,6 +84,47 @@ describe('caller',function(){
         };
         eval(readScriptSync('index-caller.js'));
         assert(funcMock.called);
+    });
+});
+
+});
+
+describe('server side',function(){
+
+    var login = function(agent, done){
+        agent
+        .get('/auth/google/callback?id=3')
+        .expect(302)
+        .expect('location', '/')
+        .expect('set-cookie', /connect\.sid=.{20,}/i, done);
+    }
+
+    var serverApp = require('../src/server.js');
+
+    describe('login, logout, and other related stuff', function(){
+        it('should have cookies when logging in', function(done){
+            //this will cause other tests to fail, but this one will help isolate issues
+            var agent = supertest.agent(serverApp);
+            login(agent, done);
+        });
+        it('should remember the user id', function(done){
+            var agent = supertest.agent(serverApp);
+            login(agent, function(){
+                agent
+                .get('/userid')
+                .expect(200)
+                .expect('3', done);
+            });
+        });
+        it('should be able to logout', function(done){
+            var agent = supertest.agent(serverApp);
+            login(agent, function(){
+                agent
+                .get('/auth/logout')
+                .expect(200, done);
+            });
+        });
+        it('should not create any more database rows after login')
     });
 });
 
