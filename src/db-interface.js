d3c42bd58f5f693a5ecce131b6bb9120df794e7d
Mark Polyakov <spamhereplease@markasoftware.com>
2016-06-17 21:36:37 -0700

    trying again with new port

--- src/db-interface.js
+++ src/db-interface.js
@@ -9,6 +9,7 @@ const conConfig = {
     user: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASS || 'password',
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || '5432',
     database: process.env.DB_NAME || 'to_the_skies_dev'
 }
 
